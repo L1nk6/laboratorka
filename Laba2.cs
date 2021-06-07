4d52a3242d06c24e025602cba85495eb6c0a4093
L1nk6 <mgvkemerovo4@gmail.com>
2021-06-07 13:40:11 +0700

    Даны положительные числа: A, B, C. Выяснить существует ли треугольник с длинами сторон А В и С.

---
+++ Laba2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double A, B, C;
+                Console.WriteLine("Введите A");
+                A = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите B");
+                B = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите C");
+                C = Convert.ToDouble(Console.ReadLine());
+                if (A < B + C)
+                {
+                    if (B < A + C)
+                    {
+                        if (C < A + B)
+                        {
+                            Console.WriteLine("Существует");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не существует");
+                        }
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+    }
+    
+}
